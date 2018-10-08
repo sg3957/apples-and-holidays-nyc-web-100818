053a10af00911022cc65896e37ce6b8906217dbf
Sydney Grant <sg3957@nyu.edu>
2018-10-08 21:35:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,4 +82,6 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
  holiday_hash.each do |season, holidays|
+   holidays.each do |holiday, supply|
+     if supply 
  
